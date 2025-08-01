55ba68ea28ac736edfca9e7614dd75f583a0ff26
Joseph-ADAMAH <adamahkouevijoseph@gmail.com>
2025-08-01 00:21:38 +0000

    Le dépot du projet (Premier commite)

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+//#include
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ nombre.h
@@ -0,0 +1,38 @@
+#ifndef NOMBRE_H_INCLUDED
+#define NOMBRE_H_INCLUDED
+
+typedef struct Liste Liste ;
+struct Liste
+{
+    int data;
+    Liste* next ;
+};
+
+int afficheMenu();
+void Construction_liste();
+void AfficherListe(Liste* tete);
+
+void longueurListe(Liste** tete);
+
+void AjoutDebut(Liste** tete);
+
+void AjoutMilieu(Liste** tete);
+
+void AjoutFin(Liste** tete);
+
+//void Supprimer(Nbre** tete, int controle);
+
+void SupprimerTete(Liste** tete);
+
+void SupprimerMilieu(Liste** tete);
+
+void SupprimerQueue(Liste** tete);
+
+void Modifier(Liste** tete);
+
+void SupprimerTout(Liste** tete);
+
+void Quitter(Liste** tete);
+
+
+#endif // NOMBRE_H_INCLUDED
